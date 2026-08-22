d0f508eaf1e128e21095fdb49327019c1c91044f
Veronica <batraveronica07@gmail.com>
2026-08-22 23:02:54 +0530

    Update content

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -7,7 +7,7 @@ const EducationItem = ({ title, institution, duration, description, icon }) => (
     <div className="edu-icon-box">
       {icon}
     </div>
-    
+
     <div className="edu-content-body">
       <h3 className="edu-title">{title}</h3>
       <h4 className="edu-institution">{institution}</h4>
@@ -39,7 +39,7 @@ const Education = () => {
     {
       title: "10th Standard (Secondary)",
       institution: "Delhi Public School",
-      duration: "2019 - 2020",
+      duration: "2020 - 2021",
       description: "Percentage: 96% | General Foundational Studies",
       icon: <FaGraduationCap />
     }
@@ -48,7 +48,7 @@ const Education = () => {
   return (
     <div className="education-container">
       {educationData.map((item, index) => (
-         <EducationItem 
+        <EducationItem
           key={index}
           title={item.title}
           institution={item.institution}
